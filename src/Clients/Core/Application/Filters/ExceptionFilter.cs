99a56b826d0f03282968d019b0933afcf41e0e98
Herbert Sampaio <hsamos7@gmail.com>
2023-06-17 22:02:42 -0300

    add filters

---
+++ src/Clients/Core/Application/Filters/ExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exc = context.Exception;
+
+            var notifications = JsonConvert.SerializeObject(new
+            {
+                error = exc.Message
+            },
+            new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+            context.Result = new ContentResult
+            {
+                Content = notifications,
+                ContentType = "application/json",
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+            };
+        }
+    }
+}
---
+++ src/Clients/Core/Application/Filters/NotificationFilter.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Filters
+{
+    public class NotificationFilter : IAsyncResultFilter
+    {
+        private readonly INotificationContext _notificationContext;
+
+        public NotificationFilter(INotificationContext notificationContext)
+        {
+            _notificationContext = notificationContext;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            if (_notificationContext.HasNotifications)
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.HttpContext.Response.ContentType = "application/json";
+
+                var notifications = JsonConvert.SerializeObject(
+                     _notificationContext.Notifications.First()
+                ,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+                _notificationContext.CleanNotification();
+
+                await context.HttpContext.Response.WriteAsync(notifications);
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
